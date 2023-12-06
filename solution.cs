b92345d4b92ba917988301f91fe7e15f721024cb
Kooreis <kooreis.official@gmail.com>
2023-12-06 09:42:03 -0600

    This update adds the 'parse' function which recursively parses a string expression from right to left, applying the addition and subtraction operations first, then the multiplication and division operations. If the string does not contain any operators, it is converted to a number.

--- solution.cs
+++ solution.cs
@@ -1,8 +1,13 @@
-function evaluate(expression) {
-    const operators = {
-        '+': (a, b) => a + b,
-        '-': (a, b) => a - b,
-        '*': (a, b) => a * b,
-        '/': (a, b) => a / b
-    };
-}
+function parse(str) {
+        for (let i = str.length - 1; i >= 0; i--) {
+            if (str[i] === '+' || str[i] === '-') {
+                return operators[str[i]](parse(str.slice(0, i)), parseFloat(str.slice(i + 1)));
+            }
+        }
+        for (let i = str.length - 1; i >= 0; i--) {
+            if (str[i] === '*' || str[i] === '/') {
+                return operators[str[i]](parse(str.slice(0, i)), parseFloat(str.slice(i + 1)));
+            }
+        }
+        return parseFloat(str);
+    }
